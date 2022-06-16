0f353fd910a227d306f0d25b530b4c07eb226d21
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-15 22:02:27 -0500

    added script, but need to hook up routes and work out bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-const displayQuote = document.getElementById('quotes');
-  const myQuotes = [
-    {
-      quote: '"Educate the masses, elevate their standard of intelligence, and you will certainly have a successful nation."',
-      author: "- Alexander Graham Bell"
-    },
-    {
-        quote: '"ipsum lorem..."',
-        author: "- Thomas Edison"
-      }
-  ];
+function buildQuotes(quoteEl) {
+
+    const quoteEl = document.createElement('quotes');
+    const myQuotes = [
+        {
+            quote: '"Educate the masses, elevate their standard of intelligence, and you will certainly have a successful nation."',
+            author: "- Alexander Graham Bell"
+        },
+        {
+            quote: '"ipsum lorem..."',
+            author: "- Thomas Edison"
+        }
+    ]
+    myQuotes.appendChild(quoteEl);
+
+};
