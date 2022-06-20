cf4073fcd54ef1b8f2d382e7ac7814155e4a5c70
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-20 17:54:34 -0500

    added quotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,27 @@ const generateQuote = function() {
             author: "- Alexander Graham Bell"
         },
         {
-            quote: '"ipsum lorem..."',
+            quote: '"Everyone needs a coach. It doesn\'t matter whether you\'re a basketball player, a tennis player, a gymnast, or a bridge player."',
+            author: "- Bill Gates"
+        },
+        {
+            quote: '“Technology is best when it brings people together.”',
+            author: "- Matt Mullenweg"
+        },
+        {
+            quote: '“It\'s not a faith in technology. It\'s faith in people.”',
+            author: "- Steve Jobs"
+        },
+        {
+            quote: '“The advance of technology is based on making it fit in so that you don\'t really even notice it.',
+            author: "- Bill Gates"
+        },
+        {
+            quote: '“Connecting to another is one of the most important things in the world and you can keep expanding that connection - one person, a family, a community, a country, a society, a culture.”',
+            author: "- Eric Fischl"
+        },
+        {
+            quote: '“What you are will show in what you do.”',
             author: "- Thomas Edison"
         }
     ];
